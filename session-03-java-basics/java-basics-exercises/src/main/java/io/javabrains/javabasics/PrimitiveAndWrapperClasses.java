688768e66792c9e2edbde382c3f22d668588aef6
Konrad Bartczak <konradbartczak@onet.pl>
2024-02-05 15:34:08 +0100

    Primitives and wrapers exercise

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/PrimitiveAndWrapperClasses.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/PrimitiveAndWrapperClasses.java
@@ -3,14 +3,27 @@ package io.javabrains.javabasics;
 public class PrimitiveAndWrapperClasses {
     public static void main(String[] args) {
         // Step 1: Declare primitive variables
-
+        boolean myBoolean = true;
+        double myDouble = Double.MAX_VALUE - 1;
         // Step 2: Convert primitive variables to wrapper objects
-
+        Boolean myBooleanWrapper = myBoolean;
+        Double myDoubleWrapper = myDouble;
+        // Step 2a: Update primitive variables to their default values
+        myBoolean = false;
+        myDouble = 0;
         // Step 3: Print the values of the wrapper objects
-
+        printVariables(myBooleanWrapper, myDoubleWrapper);
         // Step 4: Convert wrapper objects back to primitive variables
-
+        myBoolean = myBooleanWrapper;
+        myDouble = myDoubleWrapper;
         // Step 5: Print the values of the primitive variables
+        printVariables(myBoolean, myDouble);
 
+    }
+
+    private static void printVariables(boolean myBoolean, double myLong) {
+        System.out.println("________________");
+        System.out.println(myBoolean);
+        System.out.println(myLong);
     }
 }
